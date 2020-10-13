343862773c1982399c65e37965553f0a1f16a672
Arbab Ali <arbabali1441@gmail.com>
2020-10-13 12:42:59 +0500

    completed till 1:51:50

--- src/VideoSidebar.js
+++ src/VideoSidebar.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import "./VideoSidebar.css";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import MessageIcon from "@material-ui/icons/Message";
+import ShareIcon from "@material-ui/icons/Share";
 
 function VideoSidebar() {
   return (
-    <div>
+    <div className="videoSidebar">
+      <div className="videoSidebar__button">
+        
+      </div>
       <h2>I am a sidebar</h2>
     </div>
-  )
+  );
 }
 
-export default VideoSidebar
+export default VideoSidebar;
